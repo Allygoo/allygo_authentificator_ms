c0c520c5e17ac5ad492483b3c8045e76cdf8c56e
mbarrerag <mbarrerag@unal.edu.co>
2025-10-03 22:03:36 -0500

    Dockerfile working

--- allygo_authentificator_ms/src/main/java/com/allygo/allygoauthenticatorms/FirebaseConfig.java
+++ allygo_authentificator_ms/src/main/java/com/allygo/allygoauthenticatorms/FirebaseConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
+import java.io.File;
 import java.io.IOException;
 
 @Configuration
@@ -13,6 +14,12 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
+        // Depuración: muestra el valor de la variable de entorno y verifica si el archivo existe
+        String credPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        System.out.println("GOOGLE_APPLICATION_CREDENTIALS: " + credPath);
+        File credentialsFile = new File(credPath);
+        System.out.println("Credenciales existen: " + credentialsFile.exists());
+
         if (FirebaseApp.getApps().isEmpty()) {
             // Toma automáticamente las credenciales de la variable de entorno GOOGLE_APPLICATION_CREDENTIALS
             FirebaseOptions options = FirebaseOptions.builder()
